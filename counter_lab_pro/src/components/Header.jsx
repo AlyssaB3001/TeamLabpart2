c7febea4d33f66cbd89c7e035e9db4ddd782dc76
Rivar Yoder <alyode01@wsc.edu>
2025-10-03 12:41:03 -0500

    added in header

---
+++ counter_lab_pro/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div style={{justifyContent: "center", textAlign: "center"}}> 
+      <header>
+        <h1>New and Improved Task List</h1>
+      </header>
+    </div>
+  );
+}
