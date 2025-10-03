a6040f3a0bd449b1e7573f2c71b84af166d58faf
Enoch Atuahene <enatua01@wsc.edu>
2025-10-03 12:45:18 -0500

    pulled in an added header from main

--- counter_lab_pro/src/App.jsx
+++ counter_lab_pro/src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header.jsx'
+import { useState } from "react";
+import AddTaskForm from "./components/AddTaskForm";
+import FilterBar from "./components/FilterBar";
+import TaskList from "./components/TaskList";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div>
+      <Header/>
+    </div>
   )
+    <>
+      <AddTaskForm onAdd={handleAdd} />
+      <FilterBar filter={filter} onChange={setFilter} />
+      <TaskList
+        tasks={tasks}
+        onToggle={handleToggle}
+        onDelete={handleDelete}
+        filter={filter}
+      />
+    </>
+  );
 }
 
 export default App;
