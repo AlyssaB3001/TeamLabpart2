d6e3f34e8760da8ebdc8a881843f19e3f03d1e2f
Enoch Atuahene <enatua01@wsc.edu>
2025-10-07 17:27:59 -0500

    implemented a goHome prop for onClick in the App.jsx; to navigate back to the homepage

--- counter_lab_pro/src/components/Header.jsx
+++ counter_lab_pro/src/components/Header.jsx
@@ -1,8 +1,9 @@
-export default function Header() {
+export default function Header({ goHome, totalTasks }) {
   return (
-    <div style={{justifyContent: "center", textAlign: "center"}}> 
+    <div className="header-container" style={{justifyContent: "center", textAlign: "center", position: "relative"}}> 
       <header>
-        <h1>New and Improved Task List</h1>
+        <h1 onClick={goHome} style={{cursor: "pointer", color: "#4a90e2" }}>New and Improved Task List</h1>
+        {totalTasks > 0 && <div className="task-count">Total Tasks: {totalTasks}</div>}
       </header>
     </div>
   );
