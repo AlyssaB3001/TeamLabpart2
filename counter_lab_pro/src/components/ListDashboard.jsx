afd8e223183c7bca6544811f8869f0ede0c17169
Enoch Atuahene <enatua01@wsc.edu>
2025-10-08 13:16:55 -0500

    added the grid class implemented in index.css here

--- counter_lab_pro/src/components/ListDashboard.jsx
+++ counter_lab_pro/src/components/ListDashboard.jsx
@@ -4,14 +4,16 @@ import AddListForm from "./AddListForm.jsx";
 
 export default function ListDashboard({groups, onAdd, deleteList, selectList}) {
   return (
-    <div style={{ display: "flex", gap: 8, justifyContent: "center"  }}>
-      <div>
+    <>
+    <div style={{justifyContent: "center", textAlign: "center", justifyItems: "center"}}>
         <h2>My Lists</h2>
         <AddListForm onAdd={onAdd} />
-        {groups.map(group => (
-          <ListCard key={group.id} group={group} deleteList={deleteList} selectList={selectList} />
-        ))}
+        <div className="my-grid">
+          {groups.map(group => (
+            <ListCard key={group.id} group={group} deleteList={deleteList} selectList={selectList} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
