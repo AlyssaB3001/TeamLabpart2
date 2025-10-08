8591de3bf9d1f4eb7743ecf9fb8fca3971283b60
Enoch Atuahene <enatua01@wsc.edu>
2025-10-08 13:15:29 -0500

    added a justify items css property

--- counter_lab_pro/src/components/Header.jsx
+++ counter_lab_pro/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div style={{justifyContent: "center", textAlign: "center"}}> 
+    <div style={{justifyContent: "center", textAlign: "center", justifyItems: "center"}}> 
       <header>
         <h1>New and Improved Task List</h1>
       </header>
